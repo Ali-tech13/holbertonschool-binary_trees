2de825c8b136ceebe7afff5b879500e03ecbee55
Ali-tech13 <3looi3445@gmail.com>
2026-08-17 10:40:14 +0300

    Add file

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -14,5 +14,4 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_postorder(tree->right, func);
 	binary_tree_postorder(tree->left, func);
 	func(tree->n);
-	return;
 }
